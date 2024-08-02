244a12b09899971565511634501169023e8aacc4
RugbySanLazzaro <sanlazzarorugby@gmail.com>
2024-08-02 19:05:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 // Per ora non c'è bisogno di molto JavaScript, ma questo file può essere utilizzato per funzioni future
 console.log("Sito Rugby Club [Nome Città] Caricato");
+document.addEventListener("DOMContentLoaded", function() {
+    var dateElements = document.querySelectorAll(".date");
+    var today = new Date();
+    var formattedDate = today.toLocaleDateString('it-IT', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
+
+    dateElements.forEach(function(element) {
+        element.textContent = formattedDate;
+    });
+});
